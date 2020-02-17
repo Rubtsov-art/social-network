f22d27cd81ee12aa3bb0fb52e0f0be0279a41b55
Павел <pasharub60@gmal.com>
2020-02-17 09:52:20 +0500

    new element in Posts
    add textarea for Posts, and create first React.createRef()

--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -4,10 +4,20 @@ import Post from './Post/Post';
 
 const Posts = (props) => {
   
-let postListItem = props.postNumber.postsData.map((p) => <Post message={p.message}/>)
+let postListItem = props.postNumber.postsData.map((p) => <Post message={p.message}/>);
+
+let newPost = React.createRef();
+
+let addPost = () => {
+  alert(newPost.current.value)
+};
 
   return (
       <section className={style.post}>
+          <div>
+            <textarea ref={newPost}></textarea>
+          </div>
+          <button onClick={addPost}>Post</button>
           <ul className={style.posts_list}>
           {postListItem}
           </ul>
