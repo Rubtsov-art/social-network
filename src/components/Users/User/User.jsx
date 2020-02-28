08bb4549a8350d44b854ee898c9ed99b5e2d2b80
Павел <pasharub60@gmal.com>
2020-02-28 14:14:31 +0500

    axios library

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import style from './User.module.css'
+import * as axios from 'axios';
+import defaultAvatar from '../../../assets/images/default-avatar.jpg'
 
 const User = (props) => {
     if (props.usersList.length === 0){
-        props.setUsers ( [
-            { id: 1, friend: true, status: 'doma', avatar: 'https://avatanplus.com/files/resources/original/5a4bf99ec0fac160b8c71446.png', name: 'Pasha', location: {city: 'Ekaterinburg', country: 'Russia'} },
-            { id: 2, friend: true, status: 'doma', avatar: 'https://avatanplus.com/files/resources/original/5a4bf99ec0fac160b8c71446.png', name: 'Irina', location: {city: 'Ekaterinburg', country: 'Russia'} },
-            { id: 3, friend: true, status: 'doma', avatar: 'https://avatanplus.com/files/resources/original/5a4bf99ec0fac160b8c71446.png', name: 'Victor', location: {city: 'Ekaterinburg', country: 'Russia'} },
-            { id: 4, friend: false, status: 'doma', avatar: 'https://avatanplus.com/files/resources/original/5a4bf99ec0fac160b8c71446.png', name: 'Anna', location: {city: 'Nursultan', country: 'Kazakhstan'} },
-        ])
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then((response) => {
+            props.setUsers (response.data.items)
+        })
     }
 
     return (
         <ul>
             {props.usersList.map ((u) => <li key={u.id}>
                 <div>
-                    <img alt='avatar' className={style.avatar} src={u.avatar}/>
+                    <img alt='avatar' className={style.avatar} src={u.photos.small != null ? u.photos.small : defaultAvatar}/>
                     {u.friend 
                     ? <button onClick={() => {props.removeFriend(u.id)}}>To enemy</button> 
                     : <button onClick={() => {props.addFriend(u.id)}}>To friend</button>}
@@ -26,8 +25,8 @@ const User = (props) => {
                         <p>{u.status}</p>
                     </span>
                     <span>
-                        <p>{u.location.city}</p>
-                        <p>{u.location.country}</p>
+                        <p>{'u.location.city'}</p>
+                        <p>{'u.location.country'}</p>
                     </span>
                 </div>
             </li>)}
