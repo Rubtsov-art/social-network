505d5e83ffc062fc4427e35a0d9935fe6390c764
Павел <pasharub60@gmal.com>
2020-03-03 10:55:57 +0500

    add container for User(component)

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,63 +1,50 @@
 import React from 'react';
 import style from './User.module.css'
-import * as axios from 'axios';
+
 import defaultAvatar from '../../../assets/images/default-avatar.jpg'
 
 
-class User extends React.Component {
+const User = (props) => {
+    
+    let pagesLength = Math.ceil(props.totalItemsCount / props.pageSize);
 
-    componentDidMount(props) {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currantPage}&count=${this.props.pageSize}`).then((response) => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalItemsCountAC(response.data.totalCount)
-        });
+    let pagesNumber = [];
+
+    for (let p = 1; p <= pagesLength; p++) {
+        pagesNumber.push(p);
     };
 
-    onPageChanged = (pageNumber) => {
-        debugger;
-        this.props.changeCurrantPageAC(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then((response) => {
-            this.props.setUsers(response.data.items)
-        });
-    };
-
-    render = () => {
-        let pagesLength = Math.ceil(this.props.totalItemsCount/this.props.pageSize);
-        let pagesNumber = [];
-        for (let p = 1; p <= pagesLength; p++) {
-            pagesNumber.push(p);
-        };
-        let pagesNumberItem = pagesNumber.map((p) => <li><button onClick={() => {return (this.onPageChanged(p))}} className={this.props.currantPage === p && style.currantPage}>{p}</button></li>);
-
-        return (
-            <div>
-                <ul className = {style.breadCramps}>
-                    {pagesNumberItem}
-                </ul>
-                <ul>
-                    {this.props.usersList.map((u) => <li key={u.id}>
-                        <div>
-                            <img alt='avatar' className={style.avatar} src={u.photos.small != null ? u.photos.small : defaultAvatar} />
-                            {u.friend
-                                ? <button onClick={() => { this.props.removeFriend(u.id) }}>To enemy</button>
-                                : <button onClick={() => { this.props.addFriend(u.id) }}>To friend</button>}
-                        </div>
-                        <div>
-                            <span>
-                                <p>{u.name}</p>
-                                <p>{u.status}</p>
-                            </span>
-                            <span>
-                                <p>{'u.location.city'}</p>
-                                <p>{'u.location.country'}</p>
-                            </span>
-                        </div>
-                    </li>)}
-                </ul>
-            </div>
+    let pagesNumberItem = pagesNumber.map((p) => <li><button onClick={() => { return (props.onPageChanged(p)) }} className={props.currantPage === p && style.currantPage}>{p}</button></li>);
+    return (
+    <div>
+        <ul className={style.breadCramps}>
+            {pagesNumberItem}
+        </ul>
+        <ul>
+            {props.usersList.map((u) => <li key={u.id}>
+                <div>
+                    <img alt='avatar' className={style.avatar} src={u.photos.small != null ? u.photos.small : defaultAvatar} />
+                    {u.friend
+                        ? <button onClick={() => { props.removeFriend(u.id) }}>To enemy</button>
+                        : <button onClick={() => { props.addFriend(u.id) }}>To friend</button>}
+                </div>
+                <div>
+                    <span>
+                        <p>{u.name}</p>
+                        <p>{u.status}</p>
+                    </span>
+                    <span>
+                        <p>{'u.location.city'}</p>
+                        <p>{'u.location.country'}</p>
+                    </span>
+                </div>
+            </li>)}
+        </ul>
+    </div>
         )
     }
-}
+
+
 
 
 export default User;
--- src/components/Users/User/UserContainer.jsx
+++ src/components/Users/User/UserContainer.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import User from './User';
+import * as axios from 'axios';
 import { friendAC, enemyAC, setUsersAC, changeCurrantPageAC, setTotalItemsCountAC } from '../../../redux/usersReducer';
+
+
+
+class UsersResponseAPI extends React.Component {
+    componentDidMount = (props) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currantPage}&count=${this.props.pageSize}`).then((response) => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalItemsCountAC(response.data.totalCount)
+        });
+    };
+
+    onPageChanged = (pageNumber) => {
+        this.props.changeCurrantPageAC(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then((response) => {
+            this.props.setUsers(response.data.items)
+        });
+    };
+    render = () => {
+    return (<User  pageSize={this.props.pageSize}
+        totalItemsCount={this.props.totalItemsCount}
+        onPageChanged={this.onPageChanged}
+        currantPage={this.props.currantPage}
+        usersList={this.props.usersList}/>)
+    }
+}
 
 let mapStateToProps = (state) => {
     return ({
@@ -32,4 +58,4 @@ let mapDispatchToProps = (dispatch) => {
     })
 };
 
-export default connect (mapStateToProps, mapDispatchToProps) (User)
+export default connect (mapStateToProps, mapDispatchToProps) (UsersResponseAPI)
