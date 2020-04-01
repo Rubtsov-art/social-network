126dc03d5e15914cec3c30e09a382ca7af34d014
Павел <pasharub60@gmal.com>
2020-04-01 13:31:16 +0500

    react hook, first hook

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './Profile.module.css'
 import PostsContainer from './Posts/PostsContainer';
 import Avatar from './Avatar/Avatar';
-import ProfileStatus from './ProfileStatus/ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatus/ProfileStatusWithHooks';
 
 
 const Profile = (props) => {
@@ -10,7 +10,7 @@ const Profile = (props) => {
   return (
     <section className={style.main_content}>
       <Avatar profile={props.profile}/>
-      <ProfileStatus status={props.status} updateUserStatus={props.updateUserStatus} />
+      <ProfileStatusWithHooks status={props.status} updateUserStatus={props.updateUserStatus} />
       <PostsContainer store={props.store}/>
     </section>
   )
---
+++ src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import style from './ProfileStatus.module.css';
+
+const ProfileStatusWithHooks = (props) => {
+
+    let [editMode, setEditMode] = useState(false);
+    let [status, setStatus] = useState(props.status);
+
+    const editModOn = () => {
+        setEditMode(true)
+    };
+
+    const editModOff = () => {
+        setEditMode(false);
+        props.updateUserStatus(status)
+    };
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
+
+    // componentDidUpdate (prevProps, prevState) {
+    //     if (prevProps.status !== this.props.status) {
+    //         this.setState ({
+    //             status: this.props.status
+    //         })
+    //     }
+    // }
+
+    return (
+        <div>
+            {editMode ?
+                <input onChange={onStatusChange} autoFocus={true} onBlur={editModOff} value={status} />
+                : <span onDoubleClick={editModOn}>{props.status || '---'}</span>
+            }
+        </div>
+    )
+};
+
+export default ProfileStatusWithHooks
