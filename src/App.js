94cbd67f6d8cea2f4894323c402d2f10e0ebc2b2
Павел <pasharub60@gmal.com>
2020-04-12 15:00:25 +0500

    React.lazy() and React.Suspense()

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import ProfileContainer from './components/Profile/ProfileContainer';
-import Settings from './components/Settings/Settings';
-import Music from './components/Music/Music';
-import Photos from './components/Photos/Photos';
 import FriendsContainer from './components/Friends/FriendsContainer';
 import Commercial from './components/Commercial/Commercial';
 import { Route, withRouter } from 'react-router-dom';
-import Login from './components/Login/Login';
-import MessagesContainer from './components/Messages/MessagesContainer';
-import UsersContainer from './components/Users/UsersContainer';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { initializeApp } from './redux/appReducer';
@@ -18,7 +11,15 @@ import Preloader from './reusingComponent/animation/Preloader';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/redux-store';
+import withReactSuspense from './components/hoc/withReactSuspense';
 
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+const MessagesContainer = React.lazy(() => import('./components/Messages/MessagesContainer'));
+const UsersContainer = React.lazy(() => import('./components/Users/UsersContainer'));
+const Login = React.lazy(() => import('./components/Login/Login'));
+const Settings = React.lazy(() => import('./components/Settings/Settings'));
+const Music = React.lazy(() => import('./components/Music/Music'));
+const Photos = React.lazy(() => import('./components/Photos/Photos'));
 
 class App extends React.Component {
 
@@ -38,18 +39,17 @@ class App extends React.Component {
           <FriendsContainer />
         </article>
         <main className='main'>
-          <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
-          <Route path='/messages' render={() => <MessagesContainer />} />
-          <Route path='/settings' component={Settings} />
-          <Route path='/music' component={Music} />
-          <Route path='/photos' component={Photos} />
-          <Route path='/users' render={() => <UsersContainer />} />
-          <Route path='/login' render={() => <Login />} />
+          <Route path='/profile/:userId?' render= {withReactSuspense(ProfileContainer)} />
+          <Route path='/messages' render={withReactSuspense(MessagesContainer)} />
+          <Route path='/settings' component={withReactSuspense(Settings)} />
+          <Route path='/music' component={withReactSuspense(Music)} />
+          <Route path='/photos' component={withReactSuspense(Photos)} />
+          <Route path='/users' render={withReactSuspense(UsersContainer)} />
+          <Route path='/login' render={withReactSuspense(Login)} />
         </main>
         <article>
           <Commercial />
         </article>
-
       </div>
     );
   }
@@ -67,10 +67,10 @@ const AppContainer = compose(
 
 const CompleteApp = (props) => {
   return (<BrowserRouter>
-          <Provider store={store}>
-           <AppContainer/>
-          </Provider>
-        </BrowserRouter>)
+    <Provider store={store}>
+      <AppContainer />
+    </Provider>
+  </BrowserRouter>)
 }
 
 export default CompleteApp
